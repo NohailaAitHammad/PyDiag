322bb72b18a49ee98f48d1654edc445b2fbf3749
Codebynah <138702299+NohailaAitHammad@users.noreply.github.com>
2026-09-04 16:26:46 +0100

    bloc

--- f.py
+++ f.py
@@ -1,6 +1,27 @@
-dict1 = {"name": "John", "job": "Developer"}
-dict2 = {"name": "Jane", "job": "Manager"}
+#
+#f = {10, 2 + 8, 2**62 + 8}
+#print(f)
+#
+#print(hash(hash(hash(10)))) # Sortie: 10
+#print(hash(2+8)) # Sortie: 10
+#print(hash(2**62 + 8))
+#  # Sortie: 10
+#print("docstring" == hash(hash("docstring")))
+#print(hash("docstring"))
+#print(hash(hash("docstring")))
 
-# Parallel iteration over keys and values
-for (k1, v1), (k2, v2) in zip(dict1.items(), dict2.items()):
-    print(k1, v1, k2, v2)
+def some(a,b):
+    if b == 0:
+        raise ValueError("Ivalid info")
+    return a/b
+
+try:
+    a = some(10, 0)
+except ValueError as e:
+    print(e)
+    raise
+
+"""
+ila khlina ghir message dial exception howa li tafficha tma fa hna ila knt chi couche akhra li rdi thdr 3la hdhci fa rh maradich t3rf achno kyn dkchi bch kndiro raise hit hiya kt3awd tpropage lina exception bi ga3 les info dialha bch hna ndiro liha handling  o dkchi 
+"""
+    
